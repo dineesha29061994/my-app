826d415f58c1506a4d270217a564715f47174357
dineesha29061994 <109737370+dineesha29061994@users.noreply.github.com>
2022-09-20 14:11:34 +0530

    Update tomcatDeploy.groovy

--- vars/tomcatDeploy.groovy
+++ vars/tomcatDeploy.groovy
@@ -1,6 +1,6 @@
 def call (ip,user){
         ssh agent ([credid]){
-        sh "scp -o StrictHostKeyChecking=no target/*.war "${user}@${ip}:/opt/tomcat9/webapps" 
+        sh "scp -o StrictHostKeyChecking=no target/*.war ${user}@${ip}:/opt/tomcat9/webapps" 
         sh "ssh ${user}@${ip} /opt/tomcat9/bin/shutdown.sh"
         sh "ssh ${user}@${ip} /opt/tomcat9/bin/startup.sh"
                 }
